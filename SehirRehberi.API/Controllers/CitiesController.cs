6bdbf265e1f42fae56c1bd0527b29345812dcdc0
saliha gokce <gb141216@outlook.com>
2021-02-23 11:34:35 +0300

    first commit

---
+++ SehirRehberi.API/Controllers/CitiesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SehirRehberi.API.Data;
+using SehirRehberi.API.Dtos;
+using AutoMapper;
+using SehirRehberi.API.Models;
+
+namespace SehirRehberi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private IAppRepository _appRepository;
+        private IMapper _mapper;
+        public CitiesController(IAppRepository appRepository, IMapper mapper)
+        {
+            _appRepository = appRepository;
+            _mapper = mapper;
+        }
+        public ActionResult GetCities()
+        {
+            //var cities = _appRepository.GetCities().Select(x=>new CityForListDto { Description=x.Description,Name=x.Name,PhotoUrl=x.Photos.FirstOrDefault(y=>y.IsMain==true).Url}).ToList();//this mapping code will writen by automapper.
+            var cities = _appRepository.GetCities();
+            var citiesToReturn = _mapper.Map<List<CityForListDto>>(cities);
+            return Ok(citiesToReturn);
+        }
+        [HttpPost]
+        [Route("add")]
+        public ActionResult Add([FromBody] City city)
+        {
+            _appRepository.Add(city);
+            _appRepository.SaveAll();
+            return Ok(city);
+        }
+        [HttpGet]
+        [Route("detail")]
+        public ActionResult GetCityById(int id)
+        {
+            var cities = _appRepository.GetCityById(id);
+            var cityToReturn = _mapper.Map<CityForDetailDto>(cities);
+            return Ok(cityToReturn);
+        }
+        [HttpGet]
+        [Route("photos")]
+        public ActionResult GetPhotosByCity(int cityId)
+        {
+            var photos = _appRepository.GetPhotosByCity(cityId);
+            return Ok(photos);
+        }
+    }
+}
---
+++ SehirRehberi.API/Controllers/ValuesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SehirRehberi.API.Data;
+
+namespace SehirRehberi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private DataContext _context;
+
+        public ValuesController(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Values()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Value(int id)
+        {
+            var values = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(values);
+        }
+
+
+
+        // GET api/values
+        //[HttpGet]
+        //public ActionResult<IEnumerable<string>> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public ActionResult<string> Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/values
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        // PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        // DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
